2c7118589dbc22f96ed9e29b37d33b1ad3d48b37
Dennis <dennis-hezel@gmx.de>
2022-08-08 15:31:19 +0200

    Update asio-grpc to v2.0.0 (#593)
    * asio-grpc: Adjust example to changes in 2.0.0

--- cmake/projects/asio-grpc/hunter.cmake
+++ cmake/projects/asio-grpc/hunter.cmake
@@ -30,6 +30,17 @@ hunter_add_version(
     0e636e89b5ba3a4b6bec07d793ecffe1c4cfada1
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    asio-grpc
+    VERSION
+    2.0.0
+    URL
+    "https://github.com/Tradias/asio-grpc/archive/refs/tags/v2.0.0.tar.gz"
+    SHA1
+    a727806a5c93c811e8f73ecb1e733efc4739d5ff
+)
+
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
 hunter_cacheable(asio-grpc)
 hunter_download(PACKAGE_NAME asio-grpc)
