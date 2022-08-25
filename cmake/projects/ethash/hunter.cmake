3b4e95cccaff8df0e9553636bc158d1a64669cb8
Paweł Bylica <chfast@gmail.com>
2022-08-25 13:22:42 +0200

    Upgrade ethash to 1.0.0 (#602)

--- cmake/projects/ethash/hunter.cmake
+++ cmake/projects/ethash/hunter.cmake
@@ -96,6 +96,17 @@ hunter_add_version(
     175767db8adbf846dc08f7751d5582f55c63877c
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    ethash
+    VERSION
+    1.0.0
+    URL
+    https://github.com/chfast/ethash/archive/v1.0.0.tar.gz
+    SHA1
+    75e64b885be0ad90f0fad8e8e718f02d4b0edac8
+)
+
 hunter_cmake_args(
     ethash
     CMAKE_ARGS
