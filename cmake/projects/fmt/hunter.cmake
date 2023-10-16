54e33f2b2b3a97fb9c32141f958cc746fddae82b
James Eggleton <JamesEggleton@users.noreply.github.com>
2023-10-16 17:29:16 +0100

    Update fmt package to 10.1.1 (#697)
    Co-authored-by: James Eggleton <james.eggleton@x2x.media>

--- cmake/projects/fmt/hunter.cmake
+++ cmake/projects/fmt/hunter.cmake
@@ -140,6 +140,17 @@ hunter_add_version(
     6c0db60f3fa7bd4cf58edc777a2408e0ddfb28b9
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    fmt
+    VERSION
+    "10.1.1"
+    URL
+    "https://github.com/fmtlib/fmt/archive/10.1.1.tar.gz"
+    SHA1
+    e94b38a9efe0d696373a3cb1300dd24f12e2dd9c
+)
+
 hunter_cmake_args(
     fmt
     CMAKE_ARGS
